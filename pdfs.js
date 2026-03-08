9afc6970b9584498871f1bde5b213fdd36d94038
khalnayakpower <vinayaknetwork08@gmail.com>
2026-03-08 14:48:43 +0530

    Update PDF entries in pdfs.js
    Removed an outdated PDF entry and added a new one.

--- pdfs.js
+++ pdfs.js
@@ -17,10 +17,7 @@ const PDF_LIBRARY = [
         title: "WPE FRX09 - Final",
         file: "pdfs/WPE FRX09- Final.pdf"
     },
-    {
-        title: "WPE-55 (30th Nov 25)",
-        file: "pdfs/WPE-55- 30th Nov-25.pdf"
-    },
+    
     {
         title: "WPE-55 Brochure FINAL (28-11-19)",
         file: "pdfs/WPE-55-Brochure-FINAL-28-11-19.pdf"
@@ -36,6 +33,10 @@ const PDF_LIBRARY = [
     {
         title: "WPE55 Pipe Collar - New",
         file: "pdfs/wpe55-pipe-collar-New.pdf"
+    },
+    {
+        title: "WPE-55 (30th Nov 25)",
+        file: "pdfs/WPE-55- 30th Nov-25.pdf"
     },
 
     // ADD MORE LIKE THIS:
