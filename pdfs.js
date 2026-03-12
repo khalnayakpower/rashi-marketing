94710e7833f6be2367f94b93f83d4ec30c11ff9e
khalnayakpower <vinayaknetwork08@gmail.com>
2026-03-12 12:35:25 +0530

    Update PDF links to external Google Drive URLs

--- pdfs.js
+++ pdfs.js
@@ -6,39 +6,110 @@
 
 const PDF_LIBRARY = [
     {
-        title: "Comparisons of Ductseal and Kraso",
-        file: "pdfs/Comparisons-of-Ductseal-and-Kraso.pdf"
+        title: "Ductseal and Kraso",
+        file: "https://drive.google.com/file/d/18IGot-0Yw3IkDFPcfz79gQQkayynLt7C/view?usp=sharing",
+        isExternalLink: true
     },
     {
         title: "DuctPlug Datasheet",
-        file: "pdfs/DuctPlug-Datasheet.pdf"
+        file: "https://drive.google.com/file/d/1P2J2A7bvp-Qa5OXPqWFbnnn42YHj4YXN/view?usp=sharing",
+        isExternalLink: true
+
     },
     {
         title: "WPE FRX09 - Final",
-        file: "pdfs/WPE FRX09- Final.pdf"
+        file: "https://drive.google.com/file/d/1aIHMvf1tP93TXtg31ndF8WaTX4CtC9LE/view?usp=sharing",
+         isExternalLink: true
     },
     
     {
         title: "WPE-55 Brochure FINAL (28-11-19)",
-        file: "pdfs/WPE-55-Brochure-FINAL-28-11-19.pdf"
+        file: "https://drive.google.com/file/d/1xilPmxTFQ9TMB9FqldBDK5EnOdAyAHv6/view?usp=sharing",
+         isExternalLink: true
     },
     {
         title: "WPE PLUG - New",
-        file: "pdfs/WPE-PLUG-New.pdf"
+        file: "https://drive.google.com/file/d/1cERRpU_DwPVF03zh7C58gGnw9E9I9fF0/view?usp=sharing",
+         isExternalLink: true
     },
     {
         title: "WPE56 Puddle Flange",
-        file: "pdfs/WPE56-Puddle-Flange.pdf"
+        file: "https://drive.google.com/file/d/1DKvPGpU8qISFtYhKvZvSYzy16tI95O20/view?usp=sharing",
+         isExternalLink: true
     },
     {
         title: "WPE55 Pipe Collar - New",
-        file: "pdfs/wpe55-pipe-collar-New.pdf"
+        file: "https://drive.google.com/file/d/1kvB-Tucq0aSXN7v0xLQeTEsWmlWRBPZ3/view?usp=sharing",
+         isExternalLink: true
     },
     {
         title: "WPE-55 (30th Nov 25)",
-        file: "pdfs/WPE-55- 30th Nov-25.pdf"
+        file: "https://drive.google.com/file/d/1B7tw-1VFnh25gb6moGcMBGTN0aMG38Xb/view?usp=sharing",
+            isExternalLink: true
     },
-
-    // ADD MORE LIKE THIS:
-    // { title: "Your Title", file: "pdfs/your-file.pdf" },
-];
+    
+    // =============================================
+    // CABLE SEALING SYSTEMS - Google Drive
+    // =============================================
+    
+    {
+        type: "heading",
+        title: "Cable Sealing Systems"
+    },
+    
+    {
+        title: "kraso catalogue cable penetrations",
+        file: "https://drive.google.com/file/d/1GNA9Vu_Kev9-Ahxh0t4t_3NBCZIxpMsg/view?usp=sharing",
+        isExternalLink: true
+    },
+    {
+        title: "kraso catalogue fbv solutions",
+        file: "https://drive.google.com/file/d/1yHCuZQSSP62CayBipzu4Bh1v4DrYWNyY/view?usp=sharing",
+        isExternalLink: true
+    },
+    {
+        title: "kraso catalogue joint tapes",
+        file: "https://drive.google.com/file/d/1i95cFn5ehV7yx_UpKcMrffBGFQSmm_1O/view?usp=sharing",
+        isExternalLink: true
+    },
+    {
+        title: "kraso catalogue pipe penetrations",
+        file: "https://drive.google.com/file/d/1xt-MmngZk3Yy6MsfAe2f2jONHtGR9bIp/view?usp=sharing",
+        isExternalLink: true
+    },
+    {
+        title: "kraso catalogue pump sumps",
+        file: "https://drive.google.com/file/d/1ada_LFVhIQuzKPBOYuHATVBXueKUr7aU/view?usp=sharing",
+        isExternalLink: true
+    },
+    {
+        title: "kraso catalogue sealing inerts",
+        file: "https://drive.google.com/file/d/1E9fw7EwlJaP_dHW3Ql0OcT4lJM4Iey5Q/view?usp=sharing",
+        isExternalLink: true
+    },
+    {
+        title: "kraso catalogue special designs",
+        file: "https://drive.google.com/file/d/16KjMrXIfmiEPat9wq0S-rSWdsQm-tUCl/view?usp=sharing",
+        isExternalLink: true
+    },
+    {
+        title: "kraso catalogue utility entries",
+        file: "https://drive.google.com/file/d/16zrpi0a0ZoQ9jstS6IpkWrhYyLor0261/view?usp=sharing",
+        isExternalLink: true
+    },
+    {
+        title: "kraso catalogue utility outlets",
+        file: "https://drive.google.com/file/d/1nd0us4QidDEmwMbr4TX0nOPcv23iHKYb/view?usp=sharing",
+        isExternalLink: true
+    },
+    {
+        title: "kraso catalogue",
+        file: "https://drive.google.com/file/d/1U-X0abNeo41sBGaGj4aqcYdKdmmCGaI0/view?usp=sharing",
+        isExternalLink: true
+    },
+    {
+        title: "M-1-KK.FR Komplett",
+        file: "https://drive.google.com/file/d/1pXc9hvErZrIj12nrAZ17vTL_fPHxGZtp/view?usp=sharing",
+        isExternalLink: true
+    },
+]; 
